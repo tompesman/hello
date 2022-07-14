dfab9605100af5c99963570dd7bc8feef62d25be
Tom Pesman <tom@9to5.software>
2022-07-14 09:18:01 +0200

    add hostname to view

--- lib/hello_web/views/layout_view.ex
+++ lib/hello_web/views/layout_view.ex
@@ -4,4 +4,9 @@ defmodule HelloWeb.LayoutView do
   # Phoenix LiveDashboard is available only in development by default,
   # so we instruct Elixir to not warn if the dashboard route is missing.
   @compile {:no_warn_undefined, {Routes, :live_dashboard_path, 2}}
+
+  def hostname() do
+    {:ok, hostname} = :inet.gethostname()
+    hostname
+  end
 end
